770b6e1e242902b3314114381191d979547bd46d
Enanyy <742216502@qq.com>
2019-07-26 15:00:12 +0800

    Terrain

---
+++ Assets/Editor/TerrainPreviewEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor (typeof (TerrainPreview))]
+public class TerrainPreviewEditor : Editor {
+
+	public override void OnInspectorGUI() {
+		TerrainPreview mapPreview = (TerrainPreview)target;
+
+		if (DrawDefaultInspector ()) {
+			if (mapPreview.autoUpdate) {
+				mapPreview.DrawMapInEditor ();
+			}
+		}
+
+		if (GUILayout.Button ("Generate")) {
+			mapPreview.DrawMapInEditor ();
+		}
+
+        if (GUILayout.Button("Generate Tree"))
+        {
+            mapPreview.GenerateTree();
+        }
+	}
+}
